fd6dc0d97e485ec238b6f36bebc2f3757589de15
segovelo <gauchosinpingo@gmail.com>
2022-05-06 20:03:28 +0100

    Example of graphQL setup. Initial commit

---
+++ src/test/java/com/segovelo/api/graphqlexample/GraphqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segovelo.api.graphqlexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
